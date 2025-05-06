13db078f546182d84a4dc6f4b6a93931c8a5cea7
Your Name <you@example.com>
2025-05-06 12:08:03 +0700

    update

--- src/main/java/com/sf/publicpage/ExternalConfigLoader.java
+++ src/main/java/com/sf/publicpage/ExternalConfigLoader.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:config.properties")  // ดึงจาก module ของ Wildfly ที่คุณตั้งไว้
+// กำหนดให้ `config-app.properties` อยู่ใน classpath ของโปรเจกต์
+@PropertySource("classpath:config-app.properties")
+// กำหนดให้ `config-common.properties` อยู่ในตำแหน่งภายนอกโปรเจกต์
+@PropertySource("file:/D:/test/wildfly-28.0.1.Final/modules/com/application/configuration/main/config-common.properties")
 public class ExternalConfigLoader {
     // ไม่มี method ก็ได้ Spring จะโหลด properties เข้ามาให้เอง
 }
